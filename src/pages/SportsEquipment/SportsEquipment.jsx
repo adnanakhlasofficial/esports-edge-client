a4a25da6ba57e5cd31ada000abd79670d70cc899
adnanakhlasofficial <official.adnanakhlas@gmail.com>
2025-01-07 10:51:22 +0600

    Equipments sort functionality fixed

--- src/pages/SportsEquipment/SportsEquipment.jsx
+++ src/pages/SportsEquipment/SportsEquipment.jsx
@@ -5,87 +5,82 @@ import { Link, useLoaderData } from "react-router-dom";
 import { useState } from "react";
 
 const SportsEquipment = () => {
-    const loadedEquipments = useLoaderData();
-    const [equipments, setEquipments] = useState(loadedEquipments || []);
+  const loadedEquipments = useLoaderData();
+  const [equipments, setEquipments] = useState(loadedEquipments || []);
 
-    const handleSortEquipment = () => {
-        const sorted = equipments.sort((prev, curr) => {
-            return prev.price - curr.price;
-        }, 0);
+  const handleSortEquipment = () => {
+    const sorted = equipments.sort((prev, curr) => {
+      return prev.price - curr.price;
+    }, 0);
 
-        setEquipments(sorted);
-    };
+    setEquipments([...sorted]);
+  };
 
-    return (
-        <HelmetProvider>
-            <Helmet>
-                <title>Sport Equipment | Sports Edge</title>
-                <link rel="canonical" href="https://www.tacobell.com/" />
-            </Helmet>
-            <div>
-                <Banner
-                    title={"All Sports Equipment"}
-                    desc={
-                        "Explore our extensive range of high-quality sports equipment, including the Autograph Bat, Adidas Brazuca, and Cricket Bat MB Malik Supreme. Each item is carefully selected to ensure top performance and durability."
-                    }
-                    img={myEquipment}
-                ></Banner>
-                <div className="wrapper my-20">
-                    <div className="text-right">
-                        <button
-                        data-tooltip-id="my-tooltip"
-                        data-tooltip-content="Ascending Order"
-                        data-tooltip-place="top" onClick={handleSortEquipment} className="btn">
-                            Sort by Price
-                        </button>
-                    </div>
-                    <table className="w-full text-left">
-                        <thead>
-                            <tr className="*:px-2 *:py-2">
-                                <th></th>
-                                <th>Name</th>
-                                <th>Price</th>
-                                <th>Rating</th>
-                                <th>Stock</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {equipments.map((equipment, idx) => (
-                                <tr
-                                    key={equipment._id}
-                                    className="divide-y-2 divide-gray-200 dark:divide-gray-700  *:px-2 *:py-3 hover:bg-zinc-200 dark:hover:bg-zinc-700 transition-colors duration-300"
-                                >
-                                    <th className="text-right">{idx + 1}</th>
-                                    <th>{equipment.name}</th>
-                                    <th>${equipment.price}</th>
-                                    <th>{equipment.rating}</th>
-                                    <th>
-                                        {equipment.stockAvailability ? (
-                                            <span className="text-green-600">
-                                                In Stock
-                                            </span>
-                                        ) : (
-                                            <span className="text-red-600">
-                                                Not in Stock
-                                            </span>
-                                        )}
-                                    </th>
-                                    <th className="text-center text-primary hover:underline underline-offset-2">
-                                        <Link
-                                            to={`/equipment/${equipment._id}`}
-                                        >
-                                            View Details
-                                        </Link>
-                                    </th>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </HelmetProvider>
-    );
+  return (
+    <HelmetProvider>
+      <Helmet>
+        <title>Sport Equipment | Sports Edge</title>
+        <link rel="canonical" href="https://www.tacobell.com/" />
+      </Helmet>
+      <div>
+        <Banner
+          title={"All Sports Equipment"}
+          desc={
+            "Explore our extensive range of high-quality sports equipment, including the Autograph Bat, Adidas Brazuca, and Cricket Bat MB Malik Supreme. Each item is carefully selected to ensure top performance and durability."
+          }
+          img={myEquipment}
+        ></Banner>
+        <div className="wrapper my-20">
+          <div className="text-right">
+            <button
+              data-tooltip-id="my-tooltip"
+              data-tooltip-content="Ascending Order"
+              data-tooltip-place="top"
+              onClick={handleSortEquipment}
+              className="btn"
+            >
+              Sort by Price
+            </button>
+          </div>
+          <table className="w-full text-left">
+            <thead>
+              <tr className="*:px-2 *:py-2">
+                <th></th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Rating</th>
+                <th>Stock</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {equipments.map((equipment, idx) => (
+                <tr
+                  key={equipment._id}
+                  className="divide-y-2 divide-gray-200 dark:divide-gray-700  *:px-2 *:py-3 hover:bg-zinc-200 dark:hover:bg-zinc-700 transition-colors duration-300"
+                >
+                  <th className="text-right">{idx + 1}</th>
+                  <th>{equipment.name}</th>
+                  <th>${equipment.price}</th>
+                  <th>{equipment.rating}</th>
+                  <th>
+                    {equipment.stockAvailability ? (
+                      <span className="text-green-600">In Stock</span>
+                    ) : (
+                      <span className="text-red-600">Not in Stock</span>
+                    )}
+                  </th>
+                  <th className="text-center text-primary hover:underline underline-offset-2">
+                    <Link to={`/equipment/${equipment._id}`}>View Details</Link>
+                  </th>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </HelmetProvider>
+  );
 };
 
 export default SportsEquipment;
